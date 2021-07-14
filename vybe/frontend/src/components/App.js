e13e3c54842c768f5e5f06af152457358adb0c16
Jenral <dennis_otu@live.com>
2021-07-14 12:07:24 +0000

    ++react routing for home page and room views

--- vybe/frontend/src/components/App.js
+++ vybe/frontend/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import HomePage from "./homePage";
 
 export default class App extends Component {
     constructor(props){
         super(props);
     }
     render() {
-        return <h1>Testing REACT integration...</h1>;
+        return <HomePage/>;
     }
 };
 
